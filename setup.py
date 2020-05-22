3d9809463e313269974bd3c7642109d2e2d9f2d9
fmuratori <muratorifabio96@gmail.com>
2020-05-22 13:01:22 +0200

    Changed some required modules version

--- setup.py
+++ setup.py
@@ -7,12 +7,13 @@ setup(
     package_dir = {"": "src"},
     include_package_data = True,
     install_requires = [
-        "numpy==1.18.1",
-        "opencv-python==4.2.0.34",
+        "numpy==1.18.4",
+        "opencv-python==4.1.2.30",
         "pandas==1.0.3",
-        "scikit-learn==0.23.1",
+        "scikit-learn==0.22.2.post1",
         "tensorflow==2.2.0",
-        "Keras==2.3.1"
+        "Keras==2.3.1",
+        "click==7.1.2"
     ],
     entry_points = {
         "console_scripts": [
